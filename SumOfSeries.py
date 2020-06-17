4ed9442dbb672695164676bd8669f5deb2bf103e
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-06-17 11:44:35 +0500

    Final Commit

--- SumOfSeries.py
+++ SumOfSeries.py
@@ -10,6 +10,6 @@ sum1=1
 # Using for loop and setting range.
 for i in range(2,n+1):
     sum1=sum1+((x**i)/i)
+    
+# Printing the statement.
 print("The sum of series is",round(sum1,2))
-
-# 1 + x^2/2 + x^3/3 + … x^n/n
