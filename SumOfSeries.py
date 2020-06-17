2e529d773e6a28166fcc7ff8585de6f99bccc543
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-06-17 11:44:07 +0500

    s

--- SumOfSeries.py
+++ SumOfSeries.py
@@ -6,6 +6,8 @@ n=int(input("Enter the number of terms:"))
 # Taking input from the user.
 x=int(input("Enter the value of x:"))
 sum1=1
+
+# Using for loop and setting range.
 for i in range(2,n+1):
     sum1=sum1+((x**i)/i)
 print("The sum of series is",round(sum1,2))
